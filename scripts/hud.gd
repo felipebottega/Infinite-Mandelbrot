e578ddeb137be06bd5065b709752a97c5d42e635
felipebottega <felipebottega@gmail.com>
2026-05-25 21:13:12 -0300

    fix: Fixing title blocking mouse interaction with the game

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,6 +3,11 @@ extends CanvasLayer
 
 func _ready() -> void:
 	$Version.text ="v" + ProjectSettings.get_setting("application/config/version")
+	
+	var tween = create_tween()
+	tween.tween_property($Title, "modulate", Color(1.0, 0.0, 0.0, 0.0), 5.0)
+	await tween.finished
+	remove_child($Title)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
